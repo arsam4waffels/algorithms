58f15bdcb3d23007c07377f0b9c345e766a5786b
Arsam <arsam4waffels@gmail.com>
2026-09-13 18:07:58 +0330

    Add comments to AbsoluteMax class

--- src/main/java/com/thealgorithms/maths/AbsoluteMax.java
+++ src/main/java/com/thealgorithms/maths/AbsoluteMax.java
@@ -1,23 +1,38 @@
 package com.thealgorithms.maths;
 
-public final class AbsoluteMax {
-    private AbsoluteMax() {
-    }
 
-    /**
-     * Finds the absolute maximum value among the given numbers.
-     *
-     * @param numbers The numbers to compare.
-     * @return The absolute maximum value.
-     * @throws IllegalArgumentException If the input array is empty or null.
-     */
+// final class - Inheritance is not possible.
+public final class AbsoluteMax {
+
+    // The constructor has been made private.
+    // It is not possible to create an object of the class.
+    private AbsoluteMax() {}
+
+    // Finds the absolute maximum value among the given numbers.
+    // With [varargs], you can specify any number of arguments.
     public static int getMaxValue(int... numbers) {
         if (numbers == null || numbers.length == 0) {
             throw new IllegalArgumentException("Numbers array cannot be empty or null");
         }
+        // By default, the first input is set equal to the largest absolute value.
         int absMax = numbers[0];
+
+        // The counter `i` is initially set to 1, since the first data item is already
+        // recorded by default and there is no need to compare it with itself.
         for (int i = 1; i < numbers.length; i++) {
-            if (Math.abs(numbers[i]) > Math.abs(absMax) || (Math.abs(numbers[i]) == Math.abs(absMax) && numbers[i] > absMax)) {
+
+            // [There are two possibilities]
+
+            // 1. If the absolute value of the new data point is greater than the current one,
+            // a swap should be performed.
+
+            // 2. The absolute values are equal, but since the number is positive,
+            // the positive value is selected.
+
+            if (Math.abs(numbers[i]) > Math.abs(absMax)
+                    || (Math.abs(numbers[i]) == Math.abs(absMax)
+                    && numbers[i] > absMax))
+            {
                 absMax = numbers[i];
             }
         }
